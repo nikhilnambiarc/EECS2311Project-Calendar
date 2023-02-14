6f6f88928827bb81d9331eed7208d5e8dea71672
Yaqub <yaqub399@my.yorku.ca>
2023-02-13 20:48:38 -0500

    Update CalendarEventUtil.java

---
+++ PlanPerfect/PlanPerfect/src/com/CalendarEventUtil.java
@@ -0,0 +1,15 @@
+package com;
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class CalendarEventUtil {
+    public static List<LocalDateTime> getRepeatedDates(LocalDateTime startTime, LocalDateTime endTime,int intervalInDays){
+        List<LocalDateTime> repeatedDates = new ArrayList<>();
+        LocalDateTime currentDate = startTime;
+    }
+
+    
+}
