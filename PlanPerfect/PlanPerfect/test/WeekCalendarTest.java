20ff203c53c1ebf36e8c81c9c7c7e7896b6773d3
AdilMuhammad87 <adil133@my.yorku.ca>
2023-03-10 10:13:59 -0500

    Updated WeekCalendarTest.java to add reminders

--- PlanPerfect/PlanPerfect/test/WeekCalendarTest.java
+++ PlanPerfect/PlanPerfect/test/WeekCalendarTest.java
@@ -1,12 +1,16 @@
 import com.Calendar;
 import com.CalendarEvent;
 import com.WeekCalendar;
+import com.Reminder;
 
 import javax.swing.*;
 import java.awt.*;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.ArrayList;
+
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseAdapter;
 
 public class WeekCalendarTest {
 	public static void main(String[] args) {
@@ -64,6 +68,93 @@ public class WeekCalendarTest {
             }
         });
 
+		// Create a tabbed pane where mutiple calendars will go
+		JTabbedPane calendarTab = new JTabbedPane();
+
+		// "First Calendar" in Day Calendar
+		WeekCalendar firstCalendar = new WeekCalendar(events);
+		calendarTab.addTab("First Calendar", firstCalendar);
+
+		// Add mouse listener to the tab component of "First Calendar"
+		// Allows user to edit calendar name
+		JLabel firstCalendarTab = new JLabel("First Calendar");
+		firstCalendarTab.addMouseListener(new MouseAdapter() {
+			@Override
+    		public void mouseClicked(MouseEvent e) {
+				// Double-click calendar name to edit
+        		if (e.getClickCount() == 2) {
+            	String newFirstCalendarName = JOptionPane.showInputDialog("Enter a new name for the calendar:", calendarTab.getTitleAt(0));
+            		if (newFirstCalendarName != null && !newFirstCalendarName.isEmpty()) {
+                		calendarTab.setTitleAt(0, newFirstCalendarName);
+                		firstCalendarTab.setText(newFirstCalendarName);
+            		}
+        		}
+    		}
+		});
+		// This is the first calendar tab
+		calendarTab.setTabComponentAt(0, firstCalendarTab);
+
+		// Button for user to add a calendar
+		JButton addCalendarBtn = new JButton("Add a Calendar");
+
+		addCalendarBtn.addActionListener(e -> {
+			// User prompted to add new calendar
+			String calendarName = JOptionPane.showInputDialog("Enter a name for the new calendar:");
+			if (calendarName == null || calendarName.isEmpty()) {
+				return;
+			}
+		
+			// New DayCalendar object for the new tab
+			WeekCalendar newCalendar = new WeekCalendar(events);
+		
+			// Event listeners for new calendar
+			newCalendar.addCalendarEventClickListener(ev -> System.out.println(ev.getCalendarEvent()));
+			newCalendar.addCalendarEmptyClickListener(ev -> {
+				System.out.println(ev.getDateTime());
+				System.out.println(Calendar.roundTime(ev.getDateTime().toLocalTime(), 30));
+			});
+		
+			int tabPosition = calendarTab.getTabCount();
+			// New calendar added to tabbed pane, along with other calendars if created
+			calendarTab.addTab(calendarName, newCalendar);
+
+    			// Rename calendar
+			JLabel newCalendarTab = new JLabel(calendarName);
+			newCalendarTab.addMouseListener(new MouseAdapter() {
+				@Override
+				public void mouseClicked(MouseEvent e) {
+					// Double-click calendar name to edit
+					if (e.getClickCount() == 2) {
+						String newCalendarName = JOptionPane.showInputDialog("Enter a new name for the calendar:", calendarTab.getTitleAt(tabPosition));
+						if (newCalendarName != null && !newCalendarName.isEmpty()) {
+							calendarTab.setTitleAt(tabPosition, newCalendarName);
+							newCalendarTab.setText(newCalendarName);
+						}
+            				}
+        			}
+    			});
+
+			calendarTab.setTabComponentAt(tabPosition, newCalendarTab);
+
+    			// Select the newly created tab
+    			calendarTab.setSelectedIndex(tabPosition);
+	
+			// Add the tabbed pane to the JFrame's content pane
+			frm.getContentPane().add(calendarTab, BorderLayout.CENTER);
+
+			// Select the newly created tab
+			calendarTab.setSelectedIndex(calendarTab.getTabCount()-1);
+		
+			// Repaint the JFrame to show the new tab
+			frm.revalidate();
+			frm.repaint();
+		});
+
+		// ADD A REMINDER
+
+		JButton addReminderButton = new JButton("Add a Reminder");
+        	addReminderButton.addActionListener(e -> new Reminder(events).setVisible(true)); // creating a new Reminder object
+
 		JPanel weekControls = new JPanel();
 		weekControls.add(prevMonthBtn);
 		weekControls.add(prevWeekBtn);
@@ -71,11 +162,12 @@ public class WeekCalendarTest {
 		weekControls.add(nextWeekBtn);
 		weekControls.add(nextMonthBtn);
 		weekControls.add(fontSize);
-        weekControls.add(fontTypes);
+        	weekControls.add(fontTypes);
+		weekControls.add(addCalendarBtn);
+		weekControls.add(addReminderButton);
 
 		frm.add(weekControls, BorderLayout.NORTH);
-
-		frm.add(cal, BorderLayout.CENTER);
+		frm.add(calendarTab, BorderLayout.CENTER);
 		frm.setSize(1000, 900);
 		frm.setVisible(true);
 		frm.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
