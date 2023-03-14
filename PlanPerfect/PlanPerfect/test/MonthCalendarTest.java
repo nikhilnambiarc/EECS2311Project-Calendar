055f0e7c5a765f72da8c0edd3ecaa4552fabec70
Nikhil Nambiar Chengat <nikhil12@my.yorku.ca>
2023-03-14 13:29:07 -0400

    month view

--- PlanPerfect/PlanPerfect/test/MonthCalendarTest.java
+++ PlanPerfect/PlanPerfect/test/MonthCalendarTest.java
@@ -1,20 +1,18 @@
-// import com.Calendar;
-// import com.CalendarEvent;
-// import com.MonthCalendar;
+import com.Calendar;
+import com.CalendarEvent;
+import com.MonthCalendar;
 
-// import javax.swing.*;
-// import java.awt.*;
-// import java.time.LocalDate;
-// import java.time.LocalTime;
-// import java.util.ArrayList;
+import javax.swing.*;
+import java.awt.*;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.ArrayList;
 
-// public class MonthCalendarTest {
-//     public static void main(String[] args) {
-// 		JFrame frm = new JFrame();
+public class MonthCalendarTest {
+    public static void main(String[] args) {
+		
+        new MonthCalendar();
 
 
-//         ArrayList<CalendarEvent> events = new ArrayList<>();
-
-//        // MonthCalendar cal = new MonthCalendar(events);
-//     }
-// }
+    }
+}
