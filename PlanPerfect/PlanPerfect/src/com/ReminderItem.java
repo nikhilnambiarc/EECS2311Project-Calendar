fa6326c94535e6233f4dfda39dfc01d546e2bd50
AdilMuhammad87 <adil133@my.yorku.ca>
2023-03-25 17:42:01 -0400

    Create ReminderItem.java
    Mark Reminder as done by clicking a checkbox

---
+++ PlanPerfect/PlanPerfect/src/com/ReminderItem.java
@@ -0,0 +1,41 @@
+package com;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+public class ReminderItem extends JPanel {
+    private Reminder reminder;
+    private ReminderPanel reminderPanel;
+    private JCheckBox checkBox;
+    private JLabel nameLabel;
+
+    public ReminderItem(Reminder reminder, ReminderPanel reminderPanel) {
+        this.reminder = reminder;
+        this.reminderPanel = reminderPanel;
+        setLayout(new FlowLayout(FlowLayout.LEFT));
+        checkBox = new JCheckBox();
+        add(checkBox);
+
+        nameLabel = new JLabel(reminder.getName());
+        nameLabel.setFont(new Font("Arial", Font.PLAIN, 16));
+        nameLabel.addMouseListener(new MouseAdapter() {
+            public void mouseClicked(MouseEvent e) {
+                if (e.getClickCount() == 2) {
+                    int index = reminderPanel.getReminders().indexOf(reminder);
+                    reminderPanel.showReminderDetails(index);
+                }
+            }
+        });
+        add(nameLabel);
+    }
+
+    public void setFontSize(int size) {
+        nameLabel.setFont(nameLabel.getFont().deriveFont((float) size));
+    }
+
+    public void setFontType(String type) {
+        nameLabel.setFont(new Font(type, nameLabel.getFont().getStyle(), nameLabel.getFont().getSize()));
+    }
+}
