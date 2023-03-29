14b0da5f15b31a0b5be7acfebce90dd2c6c1ab0b
AdilMuhammad87 <adil133@my.yorku.ca>
2023-03-29 12:34:29 -0400

    Update SoundUtils.java

--- PlanPerfect/PlanPerfect/src/com/SoundUtils.java
+++ PlanPerfect/PlanPerfect/src/com/SoundUtils.java
@@ -3,21 +3,21 @@ package com;
 import javax.sound.midi.*;
 
 public class SoundUtils {
-    public static void playBeep() {
-        try {
-            Synthesizer synthesizer = MidiSystem.getSynthesizer();
-            synthesizer.open();
-            MidiChannel[] channels = synthesizer.getChannels();
-            channels[0].noteOn(60, 100); // Play note with key 60 (C4) and velocity 100
-            try {
-                Thread.sleep(200); // Play the note for 200 milliseconds
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            channels[0].noteOff(60); // Stop playing the note
-            synthesizer.close();
-        } catch (MidiUnavailableException e) {
-            e.printStackTrace();
-        }
-    }
+   public static void playBeep() {
+       try {
+           Synthesizer synthesizer = MidiSystem.getSynthesizer();
+           synthesizer.open();
+           MidiChannel[] channels = synthesizer.getChannels();
+           channels[0].noteOn(60, 100); // Play note with key 60 (C4) and velocity 100
+           try {
+               Thread.sleep(200); // Play the note for 200 milliseconds
+           } catch (InterruptedException e) {
+               e.printStackTrace();
+           }
+           channels[0].noteOff(60); // Stop playing the note
+           synthesizer.close();
+       } catch (MidiUnavailableException e) {
+           e.printStackTrace();
+       }
+   }
 }
