ab088179c0b61c78afffb1e1c3f61375907431ba
AdilMuhammad87 <adil133@my.yorku.ca>
2023-03-29 12:30:58 -0400

    Update Reminder.java

--- PlanPerfect/PlanPerfect/src/com/Reminder.java
+++ PlanPerfect/PlanPerfect/src/com/Reminder.java
@@ -3,32 +3,41 @@ package com;
 import java.time.LocalDateTime;
 
 public class Reminder {
-    private String name;
-    private LocalDateTime dateTime;
+   private String name;
+   private LocalDateTime dateTime;
+   private boolean completed;
 
-    public Reminder(String name, LocalDateTime dateTime) {
-        this.name = name;
-        this.dateTime = dateTime;
-    }
+   public Reminder(String name, LocalDateTime dateTime) {
+       this.name = name;
+       this.dateTime = dateTime;
+       this.completed = false;
+   }
 
-    public String getName() {
-        return name;
-    }
+   public String getName() {
+       return name;
+   }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+   public void setName(String name) {
+       this.name = name;
+   }
 
-    public LocalDateTime getDateTime() {
-        return dateTime;
-    }
+   public LocalDateTime getDateTime() {
+       return dateTime;
+   }
 
-    public void setDateTime(LocalDateTime dateTime) {
-        this.dateTime = dateTime;
-    }
+   public void setDateTime(LocalDateTime dateTime) {
+       this.dateTime = dateTime;
+   }
 
-    @Override
-    public String toString() {
-        return getName();
-    }
+   @Override
+   public String toString() {
+       return getName();
+   }
+
+   public boolean isCompleted() {
+       return completed;
+   }
+   public void setCompleted(boolean completed) {
+       this.completed = completed;
+   }
 }
