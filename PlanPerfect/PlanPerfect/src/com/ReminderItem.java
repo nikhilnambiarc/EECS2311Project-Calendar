0b64449797dcd247cd17ea1a1aeefd508a02ea0f
AdilMuhammad87 <adil133@my.yorku.ca>
2023-03-29 12:33:07 -0400

    Update ReminderItem.java

--- PlanPerfect/PlanPerfect/src/com/ReminderItem.java
+++ PlanPerfect/PlanPerfect/src/com/ReminderItem.java
@@ -6,36 +6,40 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
 public class ReminderItem extends JPanel {
-    private Reminder reminder;
-    private ReminderPanel reminderPanel;
-    private JCheckBox checkBox;
-    private JLabel nameLabel;
+   private Reminder reminder;
+   private ReminderPanel reminderPanel;
+   private JCheckBox checkBox;
+   private JLabel nameLabel;
 
-    public ReminderItem(Reminder reminder, ReminderPanel reminderPanel) {
-        this.reminder = reminder;
-        this.reminderPanel = reminderPanel;
-        setLayout(new FlowLayout(FlowLayout.LEFT));
-        checkBox = new JCheckBox();
-        add(checkBox);
+   public ReminderItem(Reminder reminder, ReminderPanel reminderPanel) {
+       this.reminder = reminder;
+       this.reminderPanel = reminderPanel;
+       setLayout(new FlowLayout(FlowLayout.LEFT));
+       checkBox = new JCheckBox();
+       checkBox.setSelected(reminder.isCompleted());
+       checkBox.addActionListener(e -> {
+           reminder.setCompleted(checkBox.isSelected());
+       });
+       add(checkBox);
 
-        nameLabel = new JLabel(reminder.getName());
-        nameLabel.setFont(new Font("Arial", Font.PLAIN, 16));
-        nameLabel.addMouseListener(new MouseAdapter() {
-            public void mouseClicked(MouseEvent e) {
-                if (e.getClickCount() == 2) {
-                    int index = reminderPanel.getReminders().indexOf(reminder);
-                    reminderPanel.showReminderDetails(index);
-                }
-            }
-        });
-        add(nameLabel);
-    }
+       nameLabel = new JLabel(reminder.getName());
+       nameLabel.setFont(new Font("Arial", Font.PLAIN, 16));
+       nameLabel.addMouseListener(new MouseAdapter() {
+           public void mouseClicked(MouseEvent e) {
+               if (e.getClickCount() == 2) {
+                   int index = reminderPanel.getReminders().indexOf(reminder);
+                   reminderPanel.showReminderDetails(index);
+               }
+           }
+       });
+       add(nameLabel);
+   }
 
-    public void setFontSize(int size) {
-        nameLabel.setFont(nameLabel.getFont().deriveFont((float) size));
-    }
+   public void setFontSize(int size) {
+       nameLabel.setFont(nameLabel.getFont().deriveFont((float) size));
+   }
 
-    public void setFontType(String type) {
-        nameLabel.setFont(new Font(type, nameLabel.getFont().getStyle(), nameLabel.getFont().getSize()));
-    }
+   public void setFontType(String type) {
+       nameLabel.setFont(new Font(type, nameLabel.getFont().getStyle(), nameLabel.getFont().getSize()));
+   }
 }
