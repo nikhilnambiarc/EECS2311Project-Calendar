cf3072d40695fdecb6b0e675d82da81cd350426a
jks-sharma <122815690+jks-sharma@users.noreply.github.com>
2023-04-10 22:11:03 -0400

    Update CalendarEventClickEvent.java

--- PlanPerfect/src/com/CalendarEventClickEvent.java
+++ PlanPerfect/src/com/CalendarEventClickEvent.java
@@ -5,7 +5,7 @@ import java.awt.*;
 public class CalendarEventClickEvent extends AWTEvent {
 
     private CalendarEvent calendarEvent;
-
+//use the getDay method to get the starting date for the week and add or subtract days accordingly
     public CalendarEventClickEvent(Object source, CalendarEvent calendarEvent) {
         super(source, 0);
         this.calendarEvent = calendarEvent;
