54d94226727d635b89fb0a26332bef7eae7d92fb
jks-sharma <122815690+jks-sharma@users.noreply.github.com>
2023-04-10 22:12:03 -0400

    Update CalendarEventClickListener.java

--- PlanPerfect/src/com/CalendarEventClickListener.java
+++ PlanPerfect/src/com/CalendarEventClickListener.java
@@ -5,5 +5,7 @@ import java.util.EventListener;
 public interface CalendarEventClickListener extends EventListener {
     // Event dispatch methods
     void calendarEventClick(CalendarEventClickEvent e);
+    //returns a representation of the listener to process
+    
 }
 
